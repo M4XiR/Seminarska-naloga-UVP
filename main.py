8d8b93572adcd9950c4e6c407b2f1f3064abf6b2
Max Rupnik <maxirupnik@gmail.com>
2024-08-11 22:27:18 +0200

    popravljen program, da ne nalaga vsake strani

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# funkcije
 
 import requests
 import os
@@ -47,17 +46,18 @@ def stevilo_zavihkov():
 #        i = str(int(i)+1)
 #        ime_strani_i = "stran_z_recepti_"+f"{i}"+".html"
 #        save_frontpage(link_do_spletne_strani(i), pot, ime_strani_i)
-
-
 def funkcija_ki_vrne_seznam_linkov_do_receptov():
     seznam_vseh_linkov = []
-    for i in list(range(int(stevilo_zavihkov()))):
+    for i in list(range(int(1))):
+        
     
+    #for i in list(range(int(stevilo_zavihkov()))):
+
         i = str(int(i)+1)
         print(i)
         html = naloži_spletno_stran(link_do_spletne_strani(i))
-        
-        # poišče linke do strani receptov
+    
+    # poišče linke do strani receptov
         soup = BeautifulSoup(html, 'html.parser')
         seznam_vseh_linkov_do_receptov = soup.find_all('a', href=True, class_='group')
         for link in seznam_vseh_linkov_do_receptov:
@@ -72,7 +72,11 @@ def funkcija_ki_vrne_seznam_linkov_do_receptov():
 def ime_recepta(html):
     soup = BeautifulSoup(html, 'html.parser')
     ime = soup.find('h1', class_='font-bold text-secondary dark:text-white text-20 md:text-28 leading-normal pt-0 p-16 md:pb-0 md:p-32 pb-0 bg-white dark:bg-slate-800 rounded-t-lg')
-    return ime.text.strip()
+    if ime:
+        return ime.text.strip()
+    else:
+        return "to ni recept"
+    
 
 
 
@@ -143,7 +147,7 @@ def stevilo_besed_v_receptu(html):
         words = besedilo.split()
         stevec += len(words)
     if stevec == 0:
-        return "to ni recept", "0"
+        return "To ni recept", "0"
     else:
         return stevec, len(odstavki)
 
@@ -161,38 +165,57 @@ def energijska_vrednost(html):
 ################################################################################################
 
 
-def zapisi_v_csv():
+def main():
     seznam_linkov = funkcija_ki_vrne_seznam_linkov_do_receptov()
     števec_ni_recept = 0
+    števec_receptov=0
     with open("podatki.csv", "w", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(["Ime recepta", "Število sestavin", "Število besed v receptu", "Število odstavkov", "Težavnost",
                         "Čas priprave", "Čas kuhanja", "Skupni čas", "Vrsta kuhinje", "Energijska vrednost na porcijo"])
-        for link in seznam_linkov:            
-            html = naloži_spletno_stran(link)
-            ime_recept = ime_recepta(html)
-            st_sestavin = stevilo_sestavin(html)
-            število_besed_v_receptu, st_odstavkov = stevilo_besed_v_receptu(html)
-            težavnost = tezavnost(html)
-            priprava_čas = čas_priprave(html)
-            kuhanje_čas = čas_kuhanja(html)
-            čas_skupni = skupni_čas(html)
-            vrsta_receptov = vrsta_recepta(html)
-            energijska_vrednos = energijska_vrednost(html)
+        
+        for i in list(range(int(stevilo_zavihkov()))):
+            i = str(int(i)+1)
+            print("stran "+str(i)+" od " +str(stevilo_zavihkov()))
+            html = naloži_spletno_stran(link_do_spletne_strani(i))
+            seznam_linkov = []
+            # poišče linke do strani receptov
+            soup = BeautifulSoup(html, 'html.parser')
+            seznam_vseh_linkov_do_receptov = soup.find_all('a', href=True, class_='group')
+            for link in seznam_vseh_linkov_do_receptov:
+                seznam_linkov.append("https://okusno.je"+link.get('href'))
 
-            if število_besed_v_receptu == "to ni recept":
-                števec_ni_recept += 1
-                print(števec_ni_recept)
-                print(ime_recepta+število_besed_v_receptu)
-                pass
-            elif st_sestavin == "ta recept nima sestavin":
-                števec_ni_recept += 1
-                print(števec_ni_recept)
-            else:
-                writer.writerow([ime_recept, st_sestavin, število_besed_v_receptu, st_odstavkov,
-                                težavnost, priprava_čas, kuhanje_čas, čas_skupni, vrsta_receptov, energijska_vrednos])
+
+            for link in seznam_linkov:      
+                števec_receptov+=1      
+                html = naloži_spletno_stran(link)
+                ime_recept = ime_recepta(html)
+                st_sestavin = stevilo_sestavin(html)
+                število_besed_v_receptu, st_odstavkov = stevilo_besed_v_receptu(html)
+                težavnost = tezavnost(html)
+                priprava_čas = čas_priprave(html)
+                kuhanje_čas = čas_kuhanja(html)
+                čas_skupni = skupni_čas(html)
+                vrsta_receptov = vrsta_recepta(html)
+                energijska_vrednos = energijska_vrednost(html)
+                print(str(števec_receptov)+". "+ime_recept)
+                if število_besed_v_receptu == "to ni recept":
+                    števec_ni_recept += 1
+                    print(števec_ni_recept)
+                    print(ime_recepta+število_besed_v_receptu)
+                    pass
+                elif st_sestavin == "ta recept nima sestavin":
+                    števec_ni_recept += 1
+                    print(števec_ni_recept)
+                elif ime_recept=="to ni recept":
+                    števec_ni_recept +=1
+                    print(števec_ni_recept)
+                else:
+                    writer.writerow([ime_recept, st_sestavin, število_besed_v_receptu, st_odstavkov,
+                                    težavnost, priprava_čas, kuhanje_čas, čas_skupni, vrsta_receptov, energijska_vrednos])
                 
     print(števec_ni_recept)
 
-    
-zapisi_v_csv()
+
+
+main()
